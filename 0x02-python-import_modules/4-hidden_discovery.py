d0d286bed92edb95880f7ee2a18806da8a09fbad
JOSH <omojolajoshua@gmail.com>
2022-12-01 22:33:38 +0000

    pyhton task 5

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+#    argv = sys.argv[1:]
+
+ #   sum = 0
+    for a in hidden_4:
+        #sum += int(a)
+        print("{:s}".format(a))
