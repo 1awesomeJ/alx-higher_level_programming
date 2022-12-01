e2bd5457ffdc7ae1c0e8f8863ad5fd175d6aed79
JOSH <omojolajoshua@gmail.com>
2022-12-01 13:33:37 +0000

    python import module task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 
-import sys
-argv = sys.argv[1:]
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
 
-k = len(argv)
+    k = len(argv)
 
-if k == 1:
-    print("{} argument:".format(k))
-    i = 1
-    for a in argv:
-        print("{}: {}".format(i, a))
-        i += 1
-if k < 1:
-    print("{} arguments.".format(k))
+    if k == 1:
+        print("{} argument:".format(k))
+        i = 1
+        for a in argv:
+            print("{}: {}".format(i, a))
+            i += 1
+    if k < 1:
+        print("{} arguments.".format(k))
 
-if k > 1:
-    print("{} arguments:".format(k))
-    i = 1
-    for a in argv:
-        print("{}: {}".format(i, a))
-        i += 1
+    if k > 1:
+        print("{} arguments:".format(k))
+        i = 1
+        for a in argv:
+            print("{}: {}".format(i, a))
+            i += 1
