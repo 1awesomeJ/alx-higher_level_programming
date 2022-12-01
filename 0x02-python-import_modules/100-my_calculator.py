0cdad87e008e1a97b7c3daf90acae70b98955b0a
JOSH <omojolajoshua@gmail.com>
2022-12-01 22:54:34 +0000

    will recommit

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1 as calc
+    import sys
+    argv= sys.argv[1:]
+    k = len(argv)
+    operators = ['+', '-', '/', '*']
+    if k != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[1] == '+':
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[0]), argv[1], int(argv[2]), calc.add(int(argv[0]), int(argv[2]))))
+    elif argv[1] == '-':
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[0]), argv[1], int(argv[2]), calc.sub(int(argv[0]), int(argv[2]))))
+    elif argv[1] == '*':
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[0]), argv[1], int(argv[2]), calc.mul(int(argv[0]), int(argv[2]))))
+    elif argv[1] == '/':
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[0]), argv[1], int(argv[2]), calc.div(int(argv[0]), int(argv[2]))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
