5d86d787cc0cef951c999b0b87e7d4b513124258
JOSH <omojolajoshua@gmail.com>
2022-12-01 23:20:20 +0000

    Python import modules task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import calculator_1 as calc
     import sys
-    argv= sys.argv[1:]
+    argv = sys.argv[1:]
     k = len(argv)
     operators = ['+', '-', '/', '*']
     if k != 3:
@@ -11,12 +11,20 @@ if __name__ == "__main__":
         exit(1)
 
     if argv[1] == '+':
-        print("{:d} {:s} {:d} = {:d}".format(int(argv[0]), argv[1], int(argv[2]), calc.add(int(argv[0]), int(argv[2]))))
+        print("{:d} {:s} {:d} = {:d}".format(
+            int(argv[0]), argv[1], int(argv[2]),
+            calc.add(int(argv[0]), int(argv[2]))))
     elif argv[1] == '-':
-        print("{:d} {:s} {:d} = {:d}".format(int(argv[0]), argv[1], int(argv[2]), calc.sub(int(argv[0]), int(argv[2]))))
+        print("{:d} {:s} {:d} = {:d}".format(
+            int(argv[0]), argv[1], int(argv[2]),
+            calc.sub(int(argv[0]), int(argv[2]))))
     elif argv[1] == '*':
-        print("{:d} {:s} {:d} = {:d}".format(int(argv[0]), argv[1], int(argv[2]), calc.mul(int(argv[0]), int(argv[2]))))
+        print("{:d} {:s} {:d} = {:d}".format(
+            int(argv[0]), argv[1], int(argv[2]),
+            calc.mul(int(argv[0]), int(argv[2]))))
     elif argv[1] == '/':
-        print("{:d} {:s} {:d} = {:d}".format(int(argv[0]), argv[1], int(argv[2]), calc.div(int(argv[0]), int(argv[2]))))
+        print("{:d} {:s} {:d} = {:d}".format(
+            int(argv[0]), argv[1], int(argv[2]),
+            calc.div(int(argv[0]), int(argv[2]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
