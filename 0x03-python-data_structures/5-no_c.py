f805640ec333d98ac61be64397e0b0dfd66939f9
JOSH <omojolajoshua@gmail.com>
2022-12-04 11:54:10 +0000

    python Data structure task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.replace('c', '')
-    my_string = my_string.replace('C', '')
-    return my_string
+    p = []
+    i = 0
+    while i < len(my_string):
+        if my_string[i] == "c" or my_string[i] == "C":
+            i += 1
+            continue
+        p.append(my_string[i])
+        i += 1
+    c = ""
+    i = 0
+    while i < len(p):
+        c += p[i]
+        i += 1
+    return c
