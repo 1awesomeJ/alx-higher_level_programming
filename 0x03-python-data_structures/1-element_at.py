d017ec36fb917dc231ce280a9960ee235d2e0c80
JOSH <omojolajoshua@gmail.com>
2022-12-04 10:40:56 +0000

    python DS task 3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,6 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     k = len(my_list)
-    if idx >= k or idx < -k:
-# the or part is not necessary cos negative indexes have been handled on line3
+    if idx >= k:
         return None
     return my_list[idx]
---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    k = len(my_list)
+    i = k - 1
+    while i >= 0:
+        print("{:d}".format(my_list[i]))
+        i -= 1
