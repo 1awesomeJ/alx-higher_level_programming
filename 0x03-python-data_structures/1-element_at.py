e331431f90696cf84d43c2ee571f79fcf53f3168
JOSH <omojolajoshua@gmail.com>
2022-12-04 10:29:33 +0000

    python DS task 2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,6 @@ def element_at(my_list, idx):
         return None
     k = len(my_list)
     if idx >= k or idx < -k:
+# the or part is not necessary cos negative indexes have been handled on line3
         return None
     return my_list[idx]
