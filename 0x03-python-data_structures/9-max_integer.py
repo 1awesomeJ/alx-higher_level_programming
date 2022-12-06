7e3424220399834d9c74cac53a670feebfcee23d
JOSH <omojolajoshua@gmail.com>
2022-12-06 00:12:31 +0000

    python task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max = my_list[0]
+    for a in my_list:
+        if a > max:
+            max = a
+
+    return max
