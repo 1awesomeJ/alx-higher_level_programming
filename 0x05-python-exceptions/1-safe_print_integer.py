363dd30fb1aa62ee8872a67c8818586274929268
JOSH <omojolajoshua@gmail.com>
2022-12-20 03:53:07 +0000

    task 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         k = True
-    except ValueError:
-        k = False
-    except NameError:
+    except(NameError, TypeError, ValueError):
         k = False
     return k
