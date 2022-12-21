598e4d54a005385f6768f595c99da4d6fd605810
JOSH <omojolajoshua@gmail.com>
2022-12-21 03:50:32 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains the definition of a class Square"""
+
+
+class Square:
+    """This is an empty class"""
+    def __init__(self, size):
+          self.__size = size
