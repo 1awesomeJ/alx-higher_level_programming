06e7b433bb171e3e1ff219b7ef5555ed671745a5
Josh <omojolajoshua@gmail.com>
2023-02-04 19:40:32 +0000

    fixed pycodestyle task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains a function definition"""
 
+
 def append_write(filename="", text=""):
     """This function writes a string into a text file"""
     with open(filename, "a") as k:
