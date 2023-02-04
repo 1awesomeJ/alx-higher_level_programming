e8fd39a6dc34502c4b9b268460c9276a36a71823
Josh <omojolajoshua@gmail.com>
2023-02-04 19:19:34 +0000

    fixed pycodestyle task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module contains a function definition"""
-def read_file(filename=""):
-	"""This function reads a text file and prints it to stdout"""
-	k = open(filename, "r")
-	print(k.read(), end ="")
 
+
+def read_file(filename=""):
+    """This function reads a text file and prints it to stdout"""
+    k = open(filename, "r")
+    print(k.read(), end="")
