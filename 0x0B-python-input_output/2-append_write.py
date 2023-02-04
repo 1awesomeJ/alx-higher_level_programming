242f59f97840a4671ae839aac4488e872db96281
Josh <omojolajoshua@gmail.com>
2023-02-04 19:39:11 +0000

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a function definition"""
+
+def append_write(filename="", text=""):
+    """This function writes a string into a text file"""
+    with open(filename, "a") as k:
+        num = k.write(text)
+        k.close()
+    return num
