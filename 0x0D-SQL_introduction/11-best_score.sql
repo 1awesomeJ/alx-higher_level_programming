1a1a9e782d8a4ba1a0bac36310ae57163d9d21a6
Josh <omojolajoshua@gmail.com>
2023-02-14 23:31:55 +0000

    task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- This script lists all the rows in a table in a database, and it doesn't fail even if the databse exists
 
-SELECT score, name FROM second_table ORDER BY score DESC LIMIT 2;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
