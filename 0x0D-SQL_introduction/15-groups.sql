9da3835d7e033ff0f79cc4c0ec236a7e6ef68bd6
Josh <omojolajoshua@gmail.com>
2023-02-18 10:48:11 +0000

    task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- This script lists all the rows with the same score  in a table in a database, and it doesn't fail even if the databse exists
 
 SELECT score, COUNT(*) AS number FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY score DESC;
