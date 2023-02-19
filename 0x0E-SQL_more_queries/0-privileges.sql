5d3744a9270652b1f820c32be8cbece8751fa4f6
Josh <omojolajoshua@gmail.com>
2023-02-19 04:57:37 +0000

    Thank God

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- This script shows all the permissions available to certain users.
 
 SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_22@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
